1277a05bc7b46a2df463c3fbb7228a424812044c
Iaguitow <vieiras.igs@gmail.com>
2022-01-16 21:28:14 +0000

    People Update

--- routes/people.js
+++ routes/people.js
@@ -58,16 +58,12 @@ routes.route("/people").get((req, res) => {
     new Promise((resolve, reject) => {
         try {
             const hashedPassword = req.query.password;
-
             helper.hashPassword(hashedPassword).then((password) => {
-
                 var sql = " INSERT INTO people (NAME,email,phonenumber, PASSWORD,dtnascimento,dtactive,active) ";
                 sql += " (SELECT ?,?,?,?,?,?,? FROM people p ";
                 sql += " WHERE (SELECT COUNT(email) FROM people pp WHERE pp.email ='" + req.query.email + "') = 0 ";
                 sql += " LIMIT 1); ";
-
                 var params = [req.query.name, req.query.email, req.query.phone, password, req.query.dateofbirth, req.query.dtactive, "S"];
-
                 db.query(sql, params).then((result) => {    
                     if (result.affectedRows === 0) {
                         resolve(res.send("User Already Exists"));
