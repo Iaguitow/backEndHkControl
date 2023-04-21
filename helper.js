26274511d82e1c01dd5cad1490bd0a08bdae431e
Iaguitow <vieiras.igs@gmail.com>
2023-04-21 17:10:20 +0100

    bCrypt changed to bcryptjs.

--- helper.js
+++ helper.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const db = require("./services/db");
 const jwt = require('jsonwebtoken');
 const pushNotification = require("./services/exponotification");
